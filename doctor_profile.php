2be46ada87c16eb6758b51498bbf4c160d23331e
Hunter18298 <aland.goran182@gmail.com>
2023-04-02 09:46:24 +0300

    sunday

---
+++ doctor_profile.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/466eacef29.js" crossorigin="anonymous"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&display=swap" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+    crossorigin="anonymous"></script>
+    <title>Document</title>
+    <style>
+        img{
+            
+            padding-left: 40%;
+            border-radius: 40px 40px 40px 40px !important;
+          border-collapse: separate;
+          overflow: hidden;
+    perspective: 1px;
+        }
+        .pad-to-right{
+            padding-left: 40%;
+        }
+    </style>
+</head>
+<body>
+    <section class="doctor">
+        <div class="container-fluid" >
+            <div class="row" style="background-color:white;">
+                <div class="col-lg-6">
+                    <h1>PROFILE</h1>
+                    <h5>Dr.Ali</h5><br> 
+                    <p>MBBS</p> 
+                    <p>Cihan International Hospital</p>
+                    <p>Erbil, Kurdistan, Iraq</p> <br>
+                    <h1>Speciality</h1>
+                    <div >
+                        <span class="btn btn-success">Oral Surgery</span>
+                          <span class="btn btn-success">Oral Surgery</span>
+                    </div> <br>
+                     <h1>CONTACT</h1>
+                     <p>aliomar@gmail.com</p>
+                    <p>+9647506453750</p> <br> 
+                      <h1>ADDRESS</h1>
+                     <p>Erbil, Raparin</p>
+                </div>
+                <div class="col-lg-6 justify-content-end">
+
+                    <img src="images/ali.jpg" width="70%" hight="auto"><br><br>
+                        <h5 class="pad-to-right">Master in Oral Surgery</h5><br> <br><br>
+                        <img src="images/logo.png" width="70%" hight="auto">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
